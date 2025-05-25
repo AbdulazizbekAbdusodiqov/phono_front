8994e4583f9f6288dc72e5224989bad052d32fca
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-05-25 10:30:28 +0500

    ok

--- layout/Header/Navbar.tsx
+++ layout/Header/Navbar.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import style from "./Navbar.module.scss"
+import Button from '../../components/Button/Button'
+import { FaRegEnvelopeOpen, FaRegHeart,FaRegUser,FaChevronDown } from "react-icons/fa6";
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className={style.header}>
+            <div className={style.container}>
+                <h1 className={style.h1}>Phono</h1>
+                <div className={style.right}>
+                    <div className={style.navigations}>
+                        <div>
+                            <FaRegEnvelopeOpen  color='#fff'/>
+                            Сообщения
+                        </div>
+                        <FaRegHeart  color='#fff'/>
+                        <div>
+                            <FaRegUser color='#fff'/>
+                            Ваш профиль
+                            <FaChevronDown color='#fff'/>
+                        </div>
+                    </div>
+                    <div>
+                        <Button variant="secondary">Добавить объявление</Button>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 

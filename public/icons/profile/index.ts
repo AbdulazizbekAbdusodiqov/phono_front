074593e9fa8f6936847f27c5e192835ffac8111f
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-05-29 15:01:21 +0500

    ok

--- public/icons/profile/index.ts
+++ public/icons/profile/index.ts
@@ -1,3 +1,3 @@
 export { default as PenIcon } from './src/PenIcon'
 export { default as ArrowIcon } from './src/ArrowIcon'
-export { default as SearchIcon } from './src/searchIcon'
+export { default as SearchIcon } from './src/SearchIcon'

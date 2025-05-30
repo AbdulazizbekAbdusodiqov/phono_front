3b660de327a889e3a49ea510bbbba04537b9d352
Husnidin <husnidin@Husnidins-MacBook-Air.local>
2025-05-30 14:24:46 +0500

    main_page

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 import style from "./home.module.scss";
+import { FilterIcon, SearchIconInput } from "../../public/icons/profile";
+import CategorySide from "../../components/home/category";
+import ProductSide from "../../components/home/products";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.home_input_wrapper}>
+        <div className={style.input_search}>
+          <input type="text" placeholder="Search..." />
+          <SearchIconInput />
+        </div>
+        <div className={style.action_icon}>
+          <div className={style.filter}>
+            <FilterIcon />
+          </div>
+          <div className={style.search}>Search</div>
+        </div>
+      </div>
+      <div className={style.category_wrapper}>
+        <h1 className={style.category_title}>Категории</h1>
+        <CategorySide />
+      </div>
+      <div className={style.prodcuts_wrapper}>
+        <h1 className={style.products_title}>Объявления</h1>
+        <ProductSide />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- public/icons/profile/index.ts
+++ public/icons/profile/index.ts
@@ -1,3 +1,5 @@
-export { default as PenIcon } from './src/PenIcon'
-export { default as ArrowIcon } from './src/ArrowIcon'
-export { default as SearchIcon } from './src/SearchIcon'
+export { default as PenIcon } from "./src/PenIcon";
+export { default as ArrowIcon } from "./src/ArrowIcon";
+export { default as SearchIcon } from "./src/SearchIcon";
+export { default as FilterIcon } from "./src/Filter";
+export { default as SearchIconInput } from "./src/InputSearchIcon";

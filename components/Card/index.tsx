2c9e6cdef0a54690e199386c9595235e763d3471
ANONIMVJ <viktorovjavohir@gmail.com>
2025-06-03 17:04:42 +0500

    add favorites

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,29 +1,40 @@
-import type React from "react"
-import Link from "next/link"
-import styles from "./Card.module.scss"
-import { FaHeart, FaRegHeart } from "react-icons/fa"
-import { Product } from "../../types"
+import React from "react";
+import Link from "next/link";
+import styles from "./Card.module.scss";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { Product } from "../../types";
 
 interface CardProps {
-  product: Product
+  product: Product;
+  isFavorite: boolean;
+  onToggleFavorite: (id: number) => void;
 }
 
-const Card: React.FC<CardProps> = ({ product}) => {
-  const { id, product_image, title, condition, storage, price, negotiable } = product
+const Card: React.FC<CardProps> = ({ product, isFavorite, onToggleFavorite }) => {
+  const { id, product_image, title, condition, storage, price, negotiable } = product;
 
   const handleFavoriteClick = (e: React.MouseEvent) => {
-    e.preventDefault() 
-    e.stopPropagation()
-  }
-  
+    e.preventDefault();
+    e.stopPropagation();
+    onToggleFavorite(id);
+  };
+
   return (
     <Link href={`/Profile/Edit`} className={styles.card}>
-      <img src={process.env.NEXT_PUBLIC_BASE_URL +"/" + product_image[0].url || "/placeholder.svg"} alt={title} className={styles.image} />
+      <img
+        src={
+          product_image && product_image.length > 0
+            ? `${process.env.NEXT_PUBLIC_BASE_URL}/${product_image[0].url}`
+            : "/placeholder.svg"
+        }
+        alt={title}
+        className={styles.image}
+      />
       <div className={styles.info}>
         <div className={styles.title}>
           <h3>{title}</h3>
           <div className={styles.like} onClick={handleFavoriteClick}>
-            <FaHeart color="#FF4E64" /> 
+            {isFavorite ? <FaHeart color="#FF4E64" /> : <FaRegHeart color="#999CA0" />}
           </div>
         </div>
         <p className={styles.wrapper}>
@@ -38,7 +49,7 @@ const Card: React.FC<CardProps> = ({ product}) => {
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;

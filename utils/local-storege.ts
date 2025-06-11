aa92778f908dd41d5b2da2d5f759ede6c634199c
Husnidin <husnidin@Husnidins-MacBook-Air.local>
2025-06-11 12:29:33 +0500

    search_fix

--- utils/local-storege.ts
+++ utils/local-storege.ts
@@ -4,7 +4,7 @@ export const setLocalStorage = (key: string, data: any) => {
 
 export const getLocalStorage = (key: string) => {
   const data = localStorage.getItem(key);
-  return data;
+  return data ? JSON.parse(data) : null;
 };
 
 export const removeLocalStorage = (key: string) => {
